6c970f4af4473db22bcbd2c1373427577de4967c
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 00:07:16 +1000

    The rectangle shape is made

---
+++ Graphic_editor_DK/Models/Shapes/RectangleShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Graphic_editor_DK.Models.Shapes
+{
+    public class RectangleShape : BaseShape
+    {
+        public override void Draw(DrawingContext drawingContext)
+        {
+            var rect = new Rect(StartPoint, EndPoint);
+            drawingContext.DrawRectangle(Fill, new Pen(Stroke, StrokeThickness), rect);
+        }
+
+        public override bool ContainsPoint(Point point)
+        {
+            var rect = new Rect(StartPoint, EndPoint);
+            return rect.Contains(point);
+        }
+    }
+}
