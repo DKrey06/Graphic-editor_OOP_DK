c2ee10a373136dd448ab29ffaf45ad2e116fe838
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 01:06:19 +1000

    Implementing a drawing tool for a rectangle

---
+++ Graphic_editor_DK/Models/Tools/RectangleTool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Graphic_editor_DK.Models.Shapes;
+using Graphic_editor_DK.Utilities.Enums;
+
+namespace Graphic_editor_DK.Models.Tools
+{
+    public class RectangleTool : BaseTool
+    {
+        public override ToolType ToolType => ToolType.Rectangle;
+
+        private RectangleShape _currentRectangle;
+        private bool _isDrawing;
+
+        public override void OnMouseDown(Point point, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _currentRectangle = new RectangleShape
+                {
+                    StartPoint = point,
+                    EndPoint = point,
+                    Stroke = Brushes.Black,
+                    Fill = Brushes.LightBlue,
+                    StrokeThickness = 2
+                };
+                _isDrawing = true;
+                ToolManager.OnShapeCreated?.Invoke(_currentRectangle);
+            }
+        }
+
+        public override void OnMouseMove(Point point, MouseEventArgs e)
+        {
+            if (_isDrawing && _currentRectangle != null)
+            {
+                _currentRectangle.EndPoint = point;
+                ToolManager.OnShapeUpdated?.Invoke(_currentRectangle);
+            }
+        }
+
+        public override void OnMouseUp(Point point, MouseButtonEventArgs e)
+        {
+            if (_isDrawing && _currentRectangle != null)
+            {
+                _currentRectangle.EndPoint = point;
+                _isDrawing = false;
+                ToolManager.OnShapeFinalized?.Invoke(_currentRectangle);
+            }
+        }
+    }
+}
