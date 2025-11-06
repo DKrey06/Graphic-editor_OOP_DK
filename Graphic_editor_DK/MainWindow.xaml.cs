c405d47674e0fcdd8c5719bd76576b436a93fafe
DKrey06 <mrdmitry2006@mail.ru>
2025-11-06 23:43:02 +1000

    Updated the cs for the main window

--- Graphic_editor_DK/MainWindow.xaml.cs
+++ Graphic_editor_DK/MainWindow.xaml.cs
@@ -13,11 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Graphic_editor_DK
+namespace GraphicEditor_DK
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
