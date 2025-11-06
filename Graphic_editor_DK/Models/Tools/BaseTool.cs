54eb64167757b9bea023404f0d5028d46f4adfdf
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 00:18:07 +1000

    Added a basic tool system

---
+++ Graphic_editor_DK/Models/Tools/BaseTool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Graphic_editor_DK.Utilities.Enums;
+
+namespace Graphic_editor_DK.Models.Tools
+{
+    public abstract class BaseTool
+    {
+        public abstract ToolType ToolType { get; }
+        public abstract void OnMouseDown(Point point, MouseButtonEventArgs e);
+        public abstract void OnMouseMove(Point point, MouseEventArgs e);
+        public abstract void OnMouseUp(Point point, MouseButtonEventArgs e);
+    }
+}
