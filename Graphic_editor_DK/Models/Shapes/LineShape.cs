1b4de5b809c0f53fb2941c8b88f379e8d8c7aaa6
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 00:00:13 +1000

    The line shape is made

---
+++ Graphic_editor_DK/Models/Shapes/LineShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Graphic_editor_DK.Models.Shapes
+{
+    public class LineShape : BaseShape
+    {
+        public override void Draw(DrawingContext drawingContext)
+        {
+            drawingContext.DrawLine(new Pen(Stroke, StrokeThickness), StartPoint, EndPoint);
+        }
+
+        public override bool ContainsPoint(Point point)
+        {
+            var geometry = new LineGeometry(StartPoint, EndPoint);
+            return geometry.StrokeContains(new Pen(Stroke, StrokeThickness + 5), point);
+
+        }
+    }
+}
