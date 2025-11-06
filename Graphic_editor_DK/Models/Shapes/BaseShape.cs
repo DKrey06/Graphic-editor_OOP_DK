1874deaeb5bbcdd07b94ef0f419f68154b22ac65
DKrey06 <mrdmitry2006@mail.ru>
2025-11-06 23:44:49 +1000

    Added a basic form model

---
+++ Graphic_editor_DK/Models/Shapes/BaseShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Graphic_editor_DK.Models.Shapes
+{
+    public abstract class BaseShape
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+        public Brush Stroke {  get; set; } = Brushes.Black;
+        public Brush Fill { get; set; } = Brushes.Transparent;
+        public double StrokeThickness { get; set; } = 2;
+
+        public abstract void Draw(DrawingContext drawingContext);
+        public abstract bool ContainsPoint(Point point);
+    }
+}
