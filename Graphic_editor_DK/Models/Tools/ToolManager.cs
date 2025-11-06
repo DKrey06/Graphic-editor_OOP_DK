7a871dc2ed88122d0ece873ef1bfbb2f53f55e2b
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 00:30:12 +1000

    Added a basic tool manager

---
+++ Graphic_editor_DK/Models/Tools/ToolManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphic_editor_DK.Utilities.Enums;
+
+namespace Graphic_editor_DK.Models.Tools
+{
+    public class ToolManager
+    {
+        private BaseTool _currentTool;
+
+        public BaseTool CurrentTool
+        {
+            get => _currentTool;
+            set
+            {
+                _currentTool = value;
+                ToolChanged?.Invoke();
+            }
+        }
+
+        public event Action ToolChanged;
+
+        public void SetTool(ToolType toolType)
+        {
+            CurrentTool = CreateTool(toolType);
+        }
+
+        private BaseTool CreateTool(ToolType toolType)
+        {
+            return new SelectionTool();
+        }
+    }
+}
