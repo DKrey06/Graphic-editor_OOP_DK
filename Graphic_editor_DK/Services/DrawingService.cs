bc0b21d3906e89f3fd9ef76c259a97c10c106e2b
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 21:05:49 +1000

    Fixing the architecture - creating services

---
+++ Graphic_editor_DK/Services/DrawingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphic_editor_DK.Models.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Graphic_editor_DK.Services
+{
+    public class DrawingService
+    {
+        public ObservableCollection<BaseShape> Shapes { get; } = new ObservableCollection<BaseShape>();
+
+        public void DrawShapes(DrawingContext drawingContext)
+        {
+            foreach (var shape in Shapes)
+            {
+                shape.Draw(drawingContext);
+            }
+        }
+
+        public BaseShape GetShapeAtPoint(Point point)
+        {
+            for (int i = Shapes.Count - 1; i >= 0; i--)
+            {
+                if (Shapes[i].ContainsPoint(point))
+                    return Shapes[i];
+            }
+            return null;
+        }
+
+        public void Clear()
+        {
+            Shapes.Clear();
+        }
+    }
+}
---
+++ Graphic_editor_DK/Services/FileService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.IO;
+using System.Xml.Serialization;
+using Graphic_editor_DK.Models.Shapes;
+using System.Collections.Generic;
+
+namespace Graphic_editor_DK.Services
+{
+    public class FileService
+    {
+        public void SaveProject(List<BaseShape> shapes)
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                Filter = "Graphic Editor Project (*.gep)|*.gep",
+                DefaultExt = ".gep"
+            };
+
+            if (saveDialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"Проект сохранен: {saveDialog.FileName}", "Успех",
+                              MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        public List<BaseShape> LoadProject()
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Filter = "Graphic Editor Project (*.gep)|*.gep",
+                DefaultExt = ".gep"
+            };
+
+            if (openDialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"Проект загружен: {openDialog.FileName}", "Успех",
+                              MessageBoxButton.OK, MessageBoxImage.Information);
+                return new List<BaseShape>();
+            }
+            return null;
+        }
+
+        public void ExportToImage()
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                Filter = "PNG Image (*.png)|*.png|JPEG Image (*.jpg)|*.jpg",
+                DefaultExt = ".png"
+            };
+
+            if (saveDialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"Изображение экспортировано: {saveDialog.FileName}", "Успех",
+                              MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
