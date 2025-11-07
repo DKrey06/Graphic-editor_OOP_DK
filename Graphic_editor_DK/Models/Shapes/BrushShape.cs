6e57e81f8cd067cb50e090551f7cb9839f60b27a
DKrey06 <mrdmitry2006@mail.ru>
2025-11-07 23:47:13 +1000

    Classes for brush and text have been created

---
+++ Graphic_editor_DK/Models/Shapes/BrushShape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Graphic_editor_DK.Models.Shapes
+{
+    [Serializable]
+    public class BrushShape : BaseShape
+    {
+        public List<Point> Points { get; set; } = new List<Point>();
+        public double BrushSize { get; set; } = 3;
+
+        public override void Draw(DrawingContext drawingContext)
+        {
+            if (Points.Count < 2) return;
+
+            var pen = new Pen(Stroke, BrushSize)
+            {
+                LineJoin = PenLineJoin.Round,
+                StartLineCap = PenLineCap.Round,
+                EndLineCap = PenLineCap.Round
+            };
+
+            for (int i = 0; i < Points.Count - 1; i++)
+            {
+                drawingContext.DrawLine(pen, Points[i], Points[i + 1]);
+            }
+        }
+
+        public override bool ContainsPoint(Point point)
+        {
+            foreach (var pt in Points)
+            {
+                if (Math.Abs(pt.X - point.X) < BrushSize + 5 &&
+                    Math.Abs(pt.Y - point.Y) < BrushSize + 5)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Graphic_editor_DK/Models/Shapes/TextShape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Graphic_editor_DK.Models.Shapes
+{
+    [Serializable]
+    public class TextShape : BaseShape
+    {
+        public string Text { get; set; } = "";
+        public double FontSize { get; set; } = 12;
+        public string FontFamily { get; set; } = "Arial";
+
+        public override void Draw(DrawingContext drawingContext)
+        {
+            var formattedText = new FormattedText(
+                Text,
+                System.Globalization.CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(FontFamily),
+                FontSize,
+                Stroke,
+                1.0);
+
+            drawingContext.DrawText(formattedText, StartPoint);
+        }
+
+        public override bool ContainsPoint(Point point)
+        {
+            return Math.Abs(StartPoint.X - point.X) < 100 &&
+                   Math.Abs(StartPoint.Y - point.Y) < 50;
+        }
+    }
+}
